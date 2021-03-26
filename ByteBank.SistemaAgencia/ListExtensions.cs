2c960a1879949f24943cabe75797592902b2fb2e
Matheus Lira Sartor <matheus.sartor@radixeng.com.br>
2021-03-26 12:06:14 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank.SistemaAgencia/ListExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    public static class ListExtensions
+    {
+        public static void AddVarious<T>(this List<T> list, params T[] itens)
+        {
+            foreach(T item in itens)
+            {
+                list.Add(item);
+            }
+        }
+    }
+}
---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using Humanizer;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lista<ContaCorrente> lista = new Lista<ContaCorrente>();
+
+            ContaCorrente contaDoGui = new ContaCorrente(546, 1234976);
+
+            ContaCorrente[] contas = new ContaCorrente[]
+            {
+            new ContaCorrente(874, 5679787),
+            new ContaCorrente(874, 5679754),
+            new ContaCorrente(874, 5679445),
+            new ContaCorrente(874, 5679445),
+            new ContaCorrente(874, 5679445),
+            new ContaCorrente(874, 5679445),
+            (contaDoGui),
+            new ContaCorrente(874, 5679445),
+            new ContaCorrente(874, 5679445),
+            new ContaCorrente(874, 5679445),
+            new ContaCorrente(874, 5679445),
+            new ContaCorrente(874, 5679445),
+        };
+
+            lista.AdicionarVarios(contas);
+
+            var numeros = new List<int> { 1, 2, 3, 4 };
+
+            numeros.AddVarious(5, 6, 7, 8);
+
+            numeros.Remove(3);
+
+            for(int i=0; i < numeros.Count; i++)
+            {
+                Console.WriteLine($"{numeros[i]}");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
